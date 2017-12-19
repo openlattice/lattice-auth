2036fa38a7f80a2cc782c20afea091e41c6fea0b
Hristo Oskov <hristo.oskov@gmail.com>
2017-12-18 18:03:54 -0800

    basic project setup

---
+++ config/lib/lib.config.js
@@ -0,0 +1,20 @@
+import PACKAGE from '../../package.json';
+
+const BANNER = `
+${PACKAGE.name} - v${PACKAGE.version}
+${PACKAGE.description}
+${PACKAGE.homepage}
+
+Copyright (c) 2017, OpenLattice, Inc. All rights reserved.
+`;
+
+const ENTRY_FILE_NAME = 'index.js';
+const LIB_FILE_NAME = 'lattice-auth';
+const LIB_NAMESPACE = 'LatticeAuth';
+
+export default {
+  BANNER,
+  ENTRY_FILE_NAME,
+  LIB_FILE_NAME,
+  LIB_NAMESPACE
+};
