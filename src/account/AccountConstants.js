74a0f1a09cc7ee98ea1ac8644dfa875b4f7cdd54
Katherine Bernstein <katherine@openlattice.com>
2018-11-13 10:40:23 -0800

    catch JSON parse error

--- src/account/AccountConstants.js
+++ src/account/AccountConstants.js
@@ -4,7 +4,7 @@
 
 /* eslint-disable import/prefer-default-export */
 
-const ORGANIZATION_ID :string = 'organization_id';
+const ORGANIZATION_ID :string = 'stored_organization_id';
 
 export {
   ORGANIZATION_ID
